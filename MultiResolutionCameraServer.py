e6d396ec63505b65108dd10fb971e9694aa24634
Mike Vanis <hello@mikevanis.com>
2017-09-26 15:47:08 +0100

    first commit

---
+++ MultiResolutionCameraServer.py
@@ -0,0 +1,74 @@
+import time
+import numpy as np
+import picamera
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import cv2
+from threading import Thread
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+from SocketServer import ThreadingMixIn
+
+# Multiple resolution setup
+camera = PiCamera()
+camera.resolution = (1024,768)
+camera.framerate = 30
+hiResCapture = PiRGBArray(camera)
+lowResCapture = PiRGBArray(camera, size=(320,240))
+hiResStream = camera.capture_continuous(hiResCapture, format="bgr", use_video_port=True)
+lowResStream = camera.capture_continuous(lowResCapture, format="bgr", use_video_port=True, splitter_port=2, resize=(320,240))
+
+time.sleep(0.5)
+
+class CamHandler(BaseHTTPRequestHandler):
+	def do_GET(self):
+		print self.path
+		if self.path.endswith("lrs.mjpg"):
+			self.send_response(200)
+			self.send_header('Content-type', 'multipart/x-mixed-replace; boundary=--jpgboundary')
+			self.end_headers()
+			print("Serving low res mjpg...")
+			while True:
+				lrs = lowResStream.next()
+				img = lrs.array
+				lowResCapture.truncate(0)
+				lowResCapture.seek(0)
+				r, buf = cv2.imencode(".jpg", img)
+				self.wfile.write("--jpgboundary\r\n")
+				self.send_header('Content-type', 'image/jpeg')
+				self.send_header('Content-length', str(len(buf)))
+				self.end_headers()
+				self.wfile.write(bytearray(buf))
+				self.wfile.write('\r\n')
+
+		if self.path.endswith("hrs.mjpg"):
+			self.send_response(200)
+                        self.send_header('Content-type', 'multipart/x-mixed-replace; boundary=--jpgboundary')
+                        self.end_headers()
+                        print("Serving hi res mjpg...")
+                        while True:
+                                hrs = hiResStream.next()
+                                hiresImage = hrs.array
+                                hiResCapture.truncate(0)
+                                hiResCapture.seek(0)
+                                r, buf = cv2.imencode(".jpg", hiresImage)
+                                self.wfile.write("--jpgboundary\r\n")
+                                self.send_header('Content-type', 'image/jpeg')
+                                self.send_header('Content-length', str(len(buf)))
+                                self.end_headers()
+                                self.wfile.write(bytearray(buf))
+                                self.wfile.write('\r\n')
+
+class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
+	"""Handle requests in separate threads."""
+
+def main():
+	try:
+		server = ThreadedHTTPServer(('', 9090), CamHandler)
+		print "Server started."
+		server.serve_forever()
+	except (KeyboardInterrupt, SystemExit):
+		camera.close()
+		server.socket.close()
+
+if __name__ == '__main__':
+	main()
---
+++ README.md
@@ -0,0 +1,26 @@
+# MultiResolutionCameraServer
+
+This is a unit test that captures two video streams from a Pi Camera at multiple resolutions and serves them as .mjpg files through a threaded HTTP server. Part of the Naturewatch project in collaboration with the Royal College of Art.
+
+## System requirements
+
+- OpenCV 3.1.0 or higher, as well as contrib modules.
+- Python 2.7
+- picamera + array variant
+- Raspberry Pi 3 or Zero W
+- Pi Camera module
+- 16GB SD card
+
+## Running the main script
+
+Simply run the script with Python.
+
+        python MultiResolutionCameraServer.py
+
+You can then access the high resolution stream at:
+        raspberrypi.local:9090/hrs.mjpg
+
+... and the low resolution stream at:
+        raspberrypi.local:9090/lrs.mjpg
+
+Be sure to replace `raspberrypi.local` with whatever hostname the Pi has been given. 
